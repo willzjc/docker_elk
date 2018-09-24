d8dc66b61af34e87d66e34496b2b5c052a27e96d
Will Chen <willzjc@gmail.com>
2018-09-25 02:50:26 +1000

    20180924 - renaming scripts

--- scripts/setup-logstash.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-# /usr/share/logstash/bin/logstash-plugin install logstash-filter-fix_protocol
-if /usr/share/logstash/bin/logstash-plugin list | grep logstash-filter-fix_protocol  ; then echo 'Already Installed' ; else /usr/share/logstash/bin/logstash-plugin install logstash-filter-fix_protocol ; fi
